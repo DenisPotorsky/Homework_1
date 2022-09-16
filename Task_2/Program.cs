cfa2e07e02484340930a032577a1366532bc2cac
Den <denis-s2@yandex.ru>
2022-09-16 15:15:09 +0500

    Выполнил задачу №2

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int min = a;
+int max = a;
+
+if (a > b)
+{
+    min = b;
+}
+else max = b;
+
+Console.WriteLine($"Максимальное число {max}, минимальное число {min}.");
