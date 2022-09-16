fb460a8cff8b8f60077bd4e813836d765b9ebeef
Den <denis-s2@yandex.ru>
2022-09-16 16:16:32 +0500

    Выполнил задачу №4

---
+++ Task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.WriteLine ("Введите первое число: ");
+int numbers1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите второе число: ");
+int numbers2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите третье число: ");
+int numbers3 = Convert.ToInt32(Console.ReadLine());
+
+int max = numbers1;
+
+if (numbers2 > max)
+{
+    max = numbers2;
+}
+if (numbers3 > max)
+{
+    max = numbers3;
+}
+
+Console.WriteLine();
+Console.WriteLine("Максимальное число " + max);
